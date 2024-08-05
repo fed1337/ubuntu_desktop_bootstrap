dc8878fa52a4f336d95ca505562f2a3ecd99cb14
Fedor Sedov <fedorwww@gmail.com>
2024-08-06 00:16:17 +0300

    Added ansible installation, minor fixes

--- README.md
+++ README.md
@@ -2,18 +2,19 @@
 
 
 ## After OS installation
-1. 
+1. Download, extract, make script executable 
 ```
 wget https://github.com/fed1337/ubuntu_desktop_bootstrap/archive/refs/tags/v0.2.tar.gz
 tar -xf ubuntu_desktop_bootstrap-0.2.tar.gz
 chmod +x ubuntu_desktop_bootstrap-0.2/bootstrap.sh
 ```
-2. Add .ssh directory with your keys to ubuntu_desktop_bootstrap
+2. Add .ssh directory with your keys to ubuntu_desktop_bootstrap directory
 3. Edit USER variable in bootstrap.sh accordingly to the name of your user
-4. Move to ubuntu_desktop_bootstrap directory and run script as root `sudo ./bootstrap.sh`
+4. Move to ubuntu_desktop_bootstrap directory and run the script as root `sudo ./bootstrap.sh`
 
 
 ## After script execution
+- read the message in terminal
 - reboot
 - check apps
 - perform basic configuration via UI
@@ -26,7 +27,7 @@ chmod +x ubuntu_desktop_bootstrap-0.2/bootstrap.sh
   - i2p
   - docker
   - k8s
+  - php
 - check disk issues
 - check dmesg errors  
 ```sudo dmesg --level=emerg,alert,crit,err,warn,notice,info```
-
--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,6 @@ USER=feds
 apt update -y
 apt upgrade -y
 
-
 # installing snaps
 snap install discord insomnia minuet mumble obs-studio postman qalculate skype slack teams-for-linux telegram-desktop nmap keepassxc smplayer drawio
 snap install --classic sublime-merge
@@ -21,91 +20,74 @@ snap install --classic phpstorm
 wget https://github.com/UnnoTed/wireguird/releases/download/v1.1.0/wireguird_amd64.deb
 dpkg -i ./wireguird_amd64.deb
 
-
 # kse
 wget https://github.com/kaikramer/keystore-explorer/releases/download/v5.5.3/kse_5.5.3_all.deb
 dpkg -i ./kse_5.5.3_all.deb
 
-
 # install a bunch of stuff
-apt update -y
-apt upgrade -y
-apt install libfuse2 git apt-transport-https mesa-utils mc htop vlc curl ca-certificates gnome-tweaks samba smbclient p7zip p7zip-rar rar unrar ffmpeg gnome-shell-extension-ubuntu-dock ubuntu-drivers-common xz-utils bleachbit stacer meld openvpn jq synaptic ubuntu-restricted-extras redis-tools lm-sensors gnome-shell-extension-manager gnome-shell-extensions smartmontools golang-go ipmitool build-essential gcc make perl cmake gnupg virtualbox-qt variety wireguard-tools google-chrome-stable libssl-dev python3-pip dconf-editor -y
-
+apt install libfuse2 git apt-transport-https mesa-utils mc htop vlc curl ca-certificates gnome-tweaks samba smbclient p7zip p7zip-rar rar unrar ffmpeg gnome-shell-extension-ubuntu-dock ubuntu-drivers-common xz-utils bleachbit stacer meld openvpn jq synaptic ubuntu-restricted-extras redis-tools lm-sensors gnome-shell-extension-manager gnome-shell-extensions smartmontools golang-go ipmitool build-essential gcc make perl cmake gnupg virtualbox-qt variety wireguard-tools google-chrome-stable libssl-dev python3-pip dconf-editor software-properties-common python3-argcomplete -y
 
 # symfony-cli
 curl -1sLf 'https://dl.cloudsmith.io/public/symfony/stable/setup.deb.sh' | bash
 apt update -y
 apt install symfony-cli -y
 
-
 # jetbrains toolbox
 curl -fsSL https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install/master/jetbrains-toolbox.sh | bash
-
 
 # try to repair stuff that may be broken
 apt --fix-broken install -y
 apt update --fix-missing -y
 
-
-# delete crap
-apt remove gnome-text-editor
-
-
-# crap cleaning after APT
-apt autoclean -y
-apt autoremove -y
-apt remove
-apt clean
-
-
 # jetbrains mono font
 wget https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip
-unzip JetBrainsMono-2.304.zip 
+unzip JetBrainsMono-2.304.zip
 cp -rp JetBrainsMono-2.304/ /usr/share/fonts/JetBrainsMono
 fc-cache -f -v
 
-
 # spoof-dpi
 curl -fsSL https://raw.githubusercontent.com/xvzc/SpoofDPI/main/install.sh | bash -s linux
-
 
 # gcloud
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 apt update
 apt install google-cloud-cli google-cloud-cli-gke-gcloud-auth-plugin
-#gcloud init
 
+# ansible
+add-apt-repository --yes --update ppa:ansible/ansible
+apt install ansible
+
+# crap cleaning
+apt remove gnome-text-editor
+apt autoclean -y
+apt autoremove -y
+apt remove
+apt clean
 
 # nvm
 cd ~/
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
-
 # delete skype autostart
 rm /home/$USER/snap/skype/current/.config/autostart/skypeforlinux.desktop
-
 
 # don't remember & hide recent files
 gsettings set org.gnome.desktop.privacy remember-recent-files false
 
-
 # hide starred tab
 mkdir /home/$USER/.config/nautilus/ui
-gresource extract /bin/nautilus /org/gnome/nautilus/ui/nautilus-window.ui > ~/.config/nautilus/ui/nautilus-window.ui
+gresource extract /bin/nautilus /org/gnome/nautilus/ui/nautilus-window.ui >~/.config/nautilus/ui/nautilus-window.ui
 export G_RESOURCE_OVERLAYS="/org/gnome/nautilus/ui=home/$USER/.config/nautilus/ui"
 mkdir -p /home/$USER/.config/systemd/user/dbus.service.d/
 touch /home/$USER/.config/systemd/user/dbus.service.d/environment.conf
 echo -e "[Service]
-#Environment="G_RESOURCE_OVERLAYS=/org/gnome/nautilus/ui=/home/$USER/.config/nautilus/ui"" > /home/$USER/.config/systemd/user/dbus.service.d/environment.conf
+#Environment="G_RESOURCE_OVERLAYS=/org/gnome/nautilus/ui=/home/$USER/.config/nautilus/ui"" >/home/$USER/.config/systemd/user/dbus.service.d/environment.conf
 
-
-# copying ssh keys
+# copy ssh keys
 cp -rp .ssh /home/$USER/.ssh
 chmod 0600 /home/$USER/.ssh/id*
 ssh-add
-
 
 # copy configs
 cp variety.conf /home/$USER/.config/variety/
@@ -115,5 +97,7 @@ chown $USER:$USER /home/$USER/.bash_aliases
 chmod 644 /home/$USER/.bash_aliases
 cp sensors-custom.conf /etc/sensors.d/
 
-
-echo 'Ubuntu Desktop Bootstrap done'
+echo -e "Ubuntu Desktop Bootstrap done
+IMPORTANT MANUAL STEPS:
+1. setup connection to GCP: gcloud init
+2. active ansible autocompletion: activate-global-python-argcomplete --user"
