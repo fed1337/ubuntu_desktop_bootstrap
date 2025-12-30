c9fd32f4f50fa72061145ba0c64afd45966f21c9
Fedor Sedov <fedorwww@gmail.com>
2025-12-30 15:11:02 +0200

    Automated package installations with `-y`, added `snap refresh`, updated PeaZip version, and moved PHP 8.4 setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,7 @@ USER=feds
 # updating system
 apt update
 apt upgrade -y
+snap refresh
 
 # installing snaps
 snap install discord minuet mumble postman qalculate slack telegram-desktop keepassxc smplayer
@@ -19,7 +20,13 @@ apt install libfuse2 git apt-transport-https mesa-utils mc htop vlc curl ca-cert
 
 # java stub package
 # equivs-build fake-java-provider
-apt install ./fake-java-provider_1.0_all.deb
+apt install ./fake-java-provider_1.0_all.deb -y
+
+# PHP 8.4 suitable for Symfony
+add-apt-repository ppa:ondrej/php
+apt update -y
+apt install php8.4-curl php8.4-intl php8.4-mbstring php8.4-mysql php8.4-xdebug php8.4-xml -y
+
 
 # symfony-cli
 curl -1sLf 'https://dl.cloudsmith.io/public/symfony/stable/setup.deb.sh' | bash
@@ -31,7 +38,7 @@ curl -fsSL https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install
 
 # install peazip GUI archive manager
 wget https://github.com/peazip/PeaZip/releases/download/10.8.0/peazip_10.8.0.LINUX.GTK2-1_amd64.deb
-apt install ./peazip_10.3.0.LINUX.GTK2-1_amd64.deb
+apt install ./peazip_10.8.0.LINUX.GTK2-1_amd64.deb -y
 
 # jetbrains mono font
 wget https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip
@@ -48,39 +55,30 @@ curl -fsSL https://raw.githubusercontent.com/xvzc/SpoofDPI/main/install.sh | bas
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 apt update
-apt install google-cloud-cli google-cloud-cli-gke-gcloud-auth-plugin
+apt install google-cloud-cli google-cloud-cli-gke-gcloud-auth-plugin -y
 
 # wireguard + gui
 apt install wireguard wireguard-tools
 wget https://github.com/UnnoTed/wireguird/releases/download/v1.1.0/wireguird_amd64.deb
-apt install ./wireguird_amd64.deb
+apt install ./wireguird_amd64.deb -y
 
 # kse
 wget https://github.com/kaikramer/keystore-explorer/releases/download/v5.6.0/kse_5.6.0_all.deb
-apt install ./kse_5.6.0_all.deb
-
-# zoom
-wget https://zoom.us/client/latest/zoom_amd64.deb
-apt install ./zoom_amd64.deb
+apt install ./kse_5.6.0_all.deb -y
 
 # rustdesk
 wget https://github.com/rustdesk/rustdesk/releases/download/1.4.4/rustdesk-1.4.4-x86_64.deb
-apt install ./rustdesk-1.4.4-x86_64.deb
+apt install ./rustdesk-1.4.4-x86_64.deb -y
 
 # nekoray
 wget https://github.com/MatsuriDayo/nekoray/releases/download/4.0.1/nekoray-4.0.1-2024-12-12-debian-x64.deb
-apt install ./nekoray-4.0.1-2024-12-12-debian-x64.deb
+apt install ./nekoray-4.0.1-2024-12-12-debian-x64.deb -y
 
 # lens
 curl -fsSL https://downloads.k8slens.dev/keys/gpg | gpg --dearmor | tee /usr/share/keyrings/lens-archive-keyring.gpg >/dev/null
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/lens-archive-keyring.gpg] https://downloads.k8slens.dev/apt/debian stable main" | tee /etc/apt/sources.list.d/lens.list >/dev/null
 apt update
-apt install lens
-
-# PHP 8.4 suitable for Symfony
-add-apt-repository ppa:ondrej/php
-apt update -y
-apt install php8.4-curl php8.4-intl php8.4-mbstring php8.4-mysql php8.4-xdebug php8.4-xml
+apt install lens -y
 
 # ansible
 pipx install --include-deps ansible
@@ -104,9 +102,9 @@ apt --fix-broken install -y
 apt update --fix-missing -y
 
 # crap cleaning
-apt remove gnome-text-editor
+apt remove gnome-text-editor -y
 apt autoclean
-apt autoremove
+apt autoremove -y
 rm -f ./*.deb
 
 # copy ssh keys
@@ -127,8 +125,9 @@ IMPORTANT MANUAL STEPS:
 1. setup connection to GCP
 1.1 gcloud init
 1.2 gcloud auth login
-2. Use Java installer
+2. Use Gnome setup script
 3. Use Composer installer and move the binary to /usr/local/bin/composer
 4. Use Nodejs installer
 5. Use virtualbox installer
-6. Install docker using playbook"
+6. Use Java installer
+7. Install docker using playbook"
