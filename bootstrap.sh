7c613b8278fb2011b8539cb8b941014b636bae68
Fedor Sedov <fedorwww@gmail.com>
2025-12-31 01:15:21 +0200

    fix docker install 2

--- bootstrap.sh
+++ bootstrap.sh
@@ -85,11 +85,9 @@ apt install ./nekoray-4.0.1-2024-12-12-debian-x64.deb -y
 # docker
 install -m 0755 -d /etc/apt/keyrings
 wget -qO- https://download.docker.com/linux/ubuntu/gpg
-mv ./gpg /etc/apt/keyrings/docker.asc
+mv gpg /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
-tee /etc/apt/sources.list.d/docker.list <<EOF
-deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu   noble stable
-EOF
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu   noble stable" > /etc/apt/sources.list.d/docker.list
 apt update
 apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
 groupadd docker
@@ -126,8 +124,8 @@ apt-mark hold tracker-miner-fs
 apt autoclean
 apt autoremove -y
 rm -f ./*.deb
-rf -rf ./fonts
-rf -f ./*.txt
+rm -rf ./fonts
+rm -f ./*.txt
 
 # copy ssh keys
 cp -rp .ssh /home/$USER/.ssh
