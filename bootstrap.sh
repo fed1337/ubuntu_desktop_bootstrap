ea770083a0dfc0a2de39b6fc1d3923b59ed5cb31
Fedor Sedov <fedorwww@gmail.com>
2025-12-30 17:14:13 +0200

    Replaced `curl` with `wget`

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,8 +20,6 @@ snap install --classic kubectl
 # install a bunch of stuff
 apt install libfuse2 git apt-transport-https mesa-utils mc htop vlc curl ca-certificates gnome-tweaks samba smbclient p7zip-full ffmpeg gnome-shell-extension-ubuntu-dock ubuntu-drivers-common xz-utils bleachbit meld openvpn jq synaptic ubuntu-restricted-extras redis-tools lm-sensors gnome-shell-extension-manager gnome-shell-extensions smartmontools golang-go ipmitool build-essential gcc make perl cmake gnupg virtualbox-qt variety google-chrome-stable libssl-dev python3-pip python3-argcomplete pipx dconf-editor software-properties-common dupeguru djview4 foliate nmap pdfarranger nmap zenmap libnss3-tools strawberry xchm virtualbox-ext-pack virtualbox-guest-additions-iso virtualbox-guest-utils virtualbox-qt -y
 
-export PATH=$PATH:/usr/bin:/bin:/usr/local/bin
-
 # java stub package
 # equivs-build fake-java-provider
 apt install ./fake-java-provider_1.0_all.deb -y
@@ -32,13 +30,13 @@ apt update -y
 apt install php8.4-curl php8.4-intl php8.4-mbstring php8.4-mysql php8.4-xdebug php8.4-xml -y
 
 # symfony-cli
-curl -1sLf 'https://dl.cloudsmith.io/public/symfony/stable/setup.deb.sh' | bash
+wget -qO- https://dl.cloudsmith.io/public/symfony/stable/setup.deb.sh | bash
 apt update
 apt install symfony-cli -y
 su -u $USER -c "symfony completion bash | sudo tee /etc/bash_completion.d/symfony"
 
 # jetbrains toolbox
-curl -fsSL https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install/master/jetbrains-toolbox.sh | bash
+wget -qO- https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install/master/jetbrains-toolbox.sh | bash
 
 # install peazip GUI archive manager
 wget https://github.com/peazip/PeaZip/releases/download/10.8.0/peazip_10.8.0.LINUX.GTK2-1_amd64.deb
@@ -53,10 +51,10 @@ rm -f JetBrainsMono-2.304.zip
 rm -r JetBrainsMono-2.304/
 
 # spoof-dpi
-curl -fsSL https://raw.githubusercontent.com/xvzc/SpoofDPI/main/install.sh | bash -s linux
+wget -qO- https://raw.githubusercontent.com/xvzc/SpoofDPI/main/install.sh | bash -s linux
 
 # gcloud
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+wget -qO- https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 apt update
 apt install google-cloud-cli google-cloud-cli-gke-gcloud-auth-plugin -y
@@ -79,7 +77,7 @@ wget https://github.com/MatsuriDayo/nekoray/releases/download/4.0.1/nekoray-4.0.
 apt install ./nekoray-4.0.1-2024-12-12-debian-x64.deb -y
 
 # lens
-curl -fsSL https://downloads.k8slens.dev/keys/gpg | gpg --dearmor | tee /usr/share/keyrings/lens-archive-keyring.gpg >/dev/null
+wget -qO- https://downloads.k8slens.dev/keys/gpg | gpg --dearmor | tee /usr/share/keyrings/lens-archive-keyring.gpg >/dev/null
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/lens-archive-keyring.gpg] https://downloads.k8slens.dev/apt/debian stable main" | tee /etc/apt/sources.list.d/lens.list >/dev/null
 apt update
 apt install lens -y
